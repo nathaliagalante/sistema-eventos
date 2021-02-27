ee7365a7459f54a4cd671ed8e92b84f7b4acb067
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-02-27 16:09:04 -0300

    edit

--- src/main/java/com/prog/sistemaeventos/model/Telefone.java
+++ src/main/java/com/prog/sistemaeventos/model/Telefone.java
@@ -17,6 +17,12 @@ public class Telefone {
     @ManyToOne(cascade = CascadeType.ALL)
     private Usuario usuario;
 
+    public Telefone(String ddd, String numero, Usuario usuario) {
+        this.ddd = ddd;
+        this.numero = numero;
+        this.usuario = usuario;
+    }
+
     public Long getId() {
         return id;
     }
@@ -49,5 +55,34 @@ public class Telefone {
         this.usuario = usuario;
     }
 
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Telefone other = (Telefone) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefone [ddd=" + ddd + "]";
+    }
+
 }
