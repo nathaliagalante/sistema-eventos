85f3a33e718540aa106e5d68ce586b991bf0a4de
Eduardo Miranda <eduardocdmb@gmail.com>
2021-03-08 16:05:22 -0300

    Classes RS de Grupo - Classe que ficou faltando (GrupoDetalhesEntradaRS)

---
+++ src/main/java/com/prog/sistemaeventos/controller/request/Grupo/GrupoDetalhesEntradaRS.java
@@ -0,0 +1,16 @@
+package com.prog.sistemaeventos.controller.request.Grupo;
+
+public class GrupoDetalhesEntradaRS {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    
+}
