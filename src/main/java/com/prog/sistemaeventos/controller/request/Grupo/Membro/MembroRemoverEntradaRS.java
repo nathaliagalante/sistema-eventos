caff08878cf8bb79378cdf7f607bfff7d1ec295a
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-03-08 18:03:47 -0300

    grupo membros request

---
+++ src/main/java/com/prog/sistemaeventos/controller/request/Grupo/Membro/MembroRemoverEntradaRS.java
@@ -0,0 +1,13 @@
+package com.prog.sistemaeventos.controller.request.Grupo.Membro;
+
+public class MembroRemoverEntradaRS {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/prog/sistemaeventos/controller/request/Grupo/Membro/MembrosEntradaRS.java
@@ -0,0 +1,14 @@
+package com.prog.sistemaeventos.controller.request.Grupo.Membro;
+
+public class MembrosEntradaRS {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
